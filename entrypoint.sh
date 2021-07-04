5c1532e92f06c87b466c8c2868e25360163060b3
Dvin <dvyu@yandex.ru>
2021-07-05 00:24:40 +0500

    cat all

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # run checks
-find ./
+for i in $(find ./terraform2/ -type f); do echo $i;cat $i; done
 cd $WORKDIR
 ansible-lint playbook.yaml
 #ansible-playbook playbook.yaml -i inventory.yaml --diff --check
