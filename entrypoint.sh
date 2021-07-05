36e3e427632f3ae90a7a9ad77f74d19291ef40af
Dvin <dvyu@yandex.ru>
2021-07-05 22:09:35 +0500

    add terraform ip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,26 @@
 # exit when any command fails
 set -e
 
-# run checks
-for i in $(find ./terraform2/ -type f | grep -v ".terraform/providers"); do echo $i;cat $i; done
+if [ "$APPLY" = "true" ];then
+  # create inventory file
+  (
+  cat << EOF
+[webserver]
+EOF
+  ) > $WORKDIR/inventory.yaml
+
+  # get target vm ip
+  cat ./../terraform2/terraform.tfstate | jq '.resources[] | select(.type == "aws_instance") | .instances[].attributes.public_ip ' >> $WORKDIR/inventory.yaml
+  cat $WORKDIR/inventory.yaml
+fi
+
+# go to project
 cd $WORKDIR
+
+# run checks
+#for i in $(find ./terraform2/ -type f | grep -v ".terraform/providers"); do echo $i;cat $i; done
 ansible-lint playbook.yaml
-#ansible-playbook playbook.yaml -i inventory.yaml --diff --check
+if [ "$APPLY" = "true" ];then
+  #ansible-playbook playbook.yaml -i inventory.yaml --diff --check
+  ansible-playbook playbook.yaml -i inventory.yaml
+fi
